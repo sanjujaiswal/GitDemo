ca2e3f8493d4ea8bcfc1973911e5ec00d4774e30
Sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-27 13:28:45 +0530

    Changed

--- hellojava.java
+++ hellojava.java
@@ -2,8 +2,12 @@ class hellojava{
 
 public static void main(String[]args){
 System.out.println("Hello world");
+<<<<<<< HEAD
 System.out.println("Hello java");
 
+=======
+System.out.println("Hello world");
+>>>>>>> e5ad6d961ae658d6ac0fbf298680e04a080734c3
 }
 
 }
